89c29d15e7faab61c1b99952b38a5038697e1c6f
Nikita Krenov <nikitabaram@gmail.com>
2024-11-12 19:19:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+namespace MiUA_Console
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try 
+            {
+                Write("Enter name: ");
+                int? x = null;
+                string? name = ReadLine();
+                WriteLine($"Hello, {name}!");
+                Write("Enter age: ");
+                int age = Convert.ToInt32(ReadLine() ?? "");
+                WriteLine($"You are {age} years old.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
